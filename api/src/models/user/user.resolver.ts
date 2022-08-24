c2b9a347d03585ae5061eebdac049abfa6f47588
jiro_omizu <jiro.omizu@gmail.com>
2022-08-24 20:35:53 +0700

    Move password related functions to lib

--- api/src/models/user/user.resolver.ts
+++ api/src/models/user/user.resolver.ts
@@ -1,32 +1,5 @@
-import { randomBytes, scryptSync } from "crypto";
 import type { GraphQLContext } from "../../client";
-
-const encryptPassword = (password: string, salt: string) => {
-  return scryptSync(password, salt, 32).toString("hex");
-};
-
-export const hashPassword = (password: string): string => {
-  // Any random string here (ideally should be atleast 16 bytes)
-  const salt = randomBytes(16).toString("hex");
-  console.log(salt);
-  return encryptPassword(password, salt) + salt;
-};
-
-export const matchPassword = (
-  password: string,
-  hash: string | null
-): Boolean => {
-  // hash can be null if the user is not existed
-  if (!!hash) {
-    // extract salt from the hashed string
-    // our hex password length is 32*2 = 64
-    const salt = hash.slice(64);
-    const originalPassHash = hash.slice(0, 64);
-    const currentPassHash = encryptPassword(password, salt);
-    return originalPassHash === currentPassHash;
-  }
-  return false;
-};
+import { hashPassword, matchPassword } from "../../lib/password";
 
 const userResolver = {
   Query: {
